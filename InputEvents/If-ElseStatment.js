fd3e1da978bd39261c9ad71bfbfb0cc3dafb1c78
SnehaGapat <gapatsneha95@outlook.com>
2020-11-06 21:55:35 +0530

    fourth push

---
+++ InputEvents/If-ElseStatment.js
@@ -0,0 +1,9 @@
+var age=20;
+if(age<15)
+{
+    document.write("Able To Appear Exam");
+}
+else
+{
+        Document.write("Unable to Appear");
+}
---
+++ InputEvents/If-else-IfStatment.js
@@ -0,0 +1,17 @@
+var book="Economic";
+if(book=="history")
+{
+    document.write("This is the History Book");
+}
+elseif(book=="Geography")
+{
+    document.write("This is the book of Geographic Structure");
+}
+elseif(book=="Mathematics")
+{
+document.write("This is the book of Maths");
+}
+else
+{
+document.write("This is the Book of Economics");
+}
